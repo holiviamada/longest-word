9afa6117ae3f90d6b5691d3bddd41e03912fd432
holiviamada <101402795+holiviamada@users.noreply.github.com>
2022-03-16 15:59:21 +0300

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
-# game.py
+import string
+import random
+import requests
 
-# [...]
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
 
     def is_valid(self, word):
         if not word:
@@ -11,4 +17,10 @@
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
--- tests/test_game.py
+++ tests/test_game.py
@@ -17,3 +17,9 @@
         new_game.grid = list('KWEUEAKRZ') # Forcer la grille à un scénario de test :
         self.assertIs(new_game.is_valid('SANDWICH'), False)
         self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # S'assurer que la grille n'a pas été modifiée
+# tests/test_game.py
+# [...]
+    def test_unknown_word_is_invalid(self):
+      new_game = Game()
+      new_game.grid = list('KWIENFUQW') # Forcer la grille à un scénario de test :
+      self.assertIs(new_game.is_valid('FEUN'), False)
