349b4c0d87735c971841b9288a30f370bdbcad75
holiviamada <101402795+holiviamada@users.noreply.github.com>
2022-03-16 14:43:12 +0300

    Game development with TDD

---
+++ tests/test_game.py
@@ -0,0 +1,19 @@
+# tests/test_game.py
+
+# [...]
+
+    def test_empty_word_is_invalid(self):
+        new_game = Game()
+        self.assertIs(new_game.is_valid(''), False)
+
+    def test_is_valid(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ') # Forcer la grille à un scénario de test :
+        self.assertIs(new_game.is_valid('EUREKA'), True)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # S'assurer que la grille n'a pas été modifiée
+
+    def test_is_invalid(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ') # Forcer la grille à un scénario de test :
+        self.assertIs(new_game.is_valid('SANDWICH'), False)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # S'assurer que la grille n'a pas été modifiée
