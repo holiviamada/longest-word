d9e9e1090efa24e4f9f1a94dc0c51fea9c6e6108
holiviamada <101402795+holiviamada@users.noreply.github.com>
2022-03-17 12:39:00 +0300

    Small web app wrapper around the Game

---
+++ wsgi.py
@@ -0,0 +1,22 @@
+# wsgi.py
+# pylint: disable=missing-docstring
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello world!"
+
+# wsgi.py
+# pylint: disable=missing-docstring
+
+from flask import Flask, render_template
+from game import Game
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    game = Game()
+    return render_template('home.html', grid=game.grid)
